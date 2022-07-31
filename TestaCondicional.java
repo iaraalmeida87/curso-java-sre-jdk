f913dcf6dbbb1c599108bc3f7ebcd2f6295babfe
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-31 17:59:32 -0300

    Uso de if e else

---
+++ TestaCondicional.java
@@ -0,0 +1,12 @@
+//Utilizando if e else (condicionais)
+public class TestaCondicional {
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 15;
+		if (idade >= 18) {
+			System.out.println("Você é maior de idade!");
+		} else {
+			System.out.println("Você é menor de idade!");
+		}
+	}
+}
