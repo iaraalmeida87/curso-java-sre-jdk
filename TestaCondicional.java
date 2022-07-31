1f789a2c9ee28be4cdab212d48b17d2739ba88df
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-31 20:47:31 -0300

    Acrescentando outra condicional ao código

--- TestaCondicional.java
+++ TestaCondicional.java
@@ -1,12 +1,22 @@
 //Utilizando if e else (condicionais)
 public class TestaCondicional {
+
 	public static void main(String[] args) {
 		System.out.println("testando condicionais");
-		int idade = 15;
+
+		int idade = 16;
+		int quantidadePessoas = 2;
+
 		if (idade >= 18) {
-			System.out.println("Você é maior de idade!");
+			System.out.println("Você tem mais que 18 anos.");
+			System.out.println("Seja bem vindo!");
 		} else {
-			System.out.println("Você é menor de idade!");
+			//Acrescentando outra condicional
+			if (quantidadePessoas >= 2) {
+				System.out.println("Você não tem 18, mas pode entrar, pois está acompanhado");
+			} else {
+				System.out.println("Infelizmente você não pode entrar.");
+			}
 		}
 	}
 }
