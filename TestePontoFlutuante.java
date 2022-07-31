c4f5b97cd1db17ced909c5f9b36c19aec2822a57
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-31 17:00:03 -0300

    Uso da variável double

---
+++ TestePontoFlutuante.java
@@ -0,0 +1,8 @@
+
+public class TestePontoFlutuante {
+	public static void main(String[] args) {
+		//Aqui usamos o double para declarar uma variável do tipo ponto flutuante (decimal)
+		double salario = 1250.70;
+		System.out.println("Salário é de " + salario);
+	}
+}
