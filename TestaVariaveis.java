38c790088f96ceba232640dc2aca057950b1e190
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-31 16:58:48 -0300

    Uso da variável int

---
+++ TestaVariaveis.java
@@ -0,0 +1,10 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá, novo teste!");
+		//Aqui declaramos uma variável do tipo int(número inteiro) sendo o valor amarzenado por ela o resultado inteiro da subtração de dois número inteiros
+		int idade; 
+		idade = 2022-1987;
+		System.out.println("Eu tenho " + idade + " anos.");
+	}
+}
