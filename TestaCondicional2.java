bf225641d4d1854a5451dadcec0a4ed4cd60be5d
Iara Almeida <iara.almeida.profissional@gmail.com>
2022-07-31 21:01:43 -0300

    Refatoração

--- TestaCondicional2.java
+++ TestaCondicional2.java
@@ -6,7 +6,7 @@ public class TestaCondicional2 {
 
 		int idade = 17;
 		int quantidadePessoas = 1;
-	//Utilizando o OU || para uma coida ou outra eu o E && para uma coisa e outra
+		// Utilizando o OU || para uma coida ou outra eu o E && para uma coisa e outra
 		if (idade >= 18 || quantidadePessoas >= 2) {
 			System.out.println("Seja bem vindo(a)!");
 		} else {
